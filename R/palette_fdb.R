359504e2337368edc3ec8bb4e4f432c992c52614
Dave Braze <davebraze@gmail.com>
2022-04-08 13:20:52 -0400

    aspire to my own palette(s)

---
+++ R/palette_fdb.R
@@ -0,0 +1,21 @@
+##' @title A palette
+##'
+##' @description A palette, or maybe a set of them.
+##'
+##' @details
+##' Someday I will design my own color palette. Look
+##' [here](https://blog.datawrapper.de/colors-for-data-vis-style-guides/)
+##' for some ideas about how to do that.
+##'
+##' But, I think I may just adapt some work from the MetBrewer package.
+##'
+##' @return A color palette as a vector.
+##' @export
+##' @author Dave Braze \email{davebraze@@gmail.com}
+##' @seealso \code{\link[MetBrewer]{met.brewer}}
+##' @examples
+##'
+palette_fdb <- function()
+{
+
+}
